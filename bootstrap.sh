4d6bfa643744f2e6a8eaf77bf849158f26a52779
dreboard <dre.board@gmail.com>
2017-01-08 21:53:34 -0500

    Boostrap file update

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,11 @@
 
 # Use single quotes instead of double quotes to make it work with special-character passwords
 # variables
-DBNAME='myfwk'
+SITENAME='myfwk'
+DBNAME='development'
 PASSWORD='1234'
 PROJECTFOLDER='./public'
-WPUSER='root'
+DBUSER='root'
 
 
 # update / upgrade
@@ -19,22 +20,34 @@ sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 
 sudo apt-get update
-sudo apt-get install -y php7.0-fpm php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-phpdbg php7.0-mbstring php7.0-gd php7.0-imap php7.0-ldap php7.0-pgsql php7.0-pspell php7.0-recode php7.0-snmp php7.0-tidy php7.0-dev php7.0-intl php7.0-gd php7.0-curl php7.0-zip php7.0-xml php7.0-curl php7.0-json php7.0-mcrypt
+
+##########################################################
+#				Install PHP
+##########################################################
+echo -e "\n--- Install PHP ---\n"
+sudo apt-get install -y php7.0-fpm php7.0-common php7.0-opcache php7.0-phpdbg php7.0-mbstring php7.0-gd php7.0-imap php7.0-ldap php7.0-pgsql php7.0-pspell php7.0-recode php7.0-snmp php7.0-tidy php7.0-dev php7.0-intl php7.0-gd php7.0-curl php7.0-zip php7.0-xml php7.0-curl php7.0-json php7.0-mcrypt
 
 sudo apt-get install -y php7.0-mysql
 
-# xdebug install & config
+##########################################################
+#				Install Xdebug
+##########################################################
+echo -e "\n--- Installing Xdebug ---\n"
 sudo apt-get install -y php-xdebug
 cat << EOF | sudo tee -a /etc/php/7.0/cli/conf.d/xdebug.ini
-zend_extension="zend_extension="/usr/lib/php/20160303/xdebug.so"
+zend_extension="/usr/lib/php/20160303/xdebug.so"
 xdebug.remote_enable=on
 xdebug.remote_connect_back=on
 EOF
 
+
+##########################################################
+#				Install MySQL
+##########################################################
 # install mysql and give password to installer
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server
+sudo apt-get -y install mysql-server mysql-client
 
 # install phpmyadmin and give password(s) to installer
 # for simplicity I'm using the same password for mysql and phpmyadmin
@@ -45,41 +58,57 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 sudo apt-get -y install phpmyadmin
 
+echo -e "\n--- Setting up our MySQL user and db ---\n"
+mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME" >> /vagrant/vm_build.log 2>&1
+mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'" > /vagrant/vm_build.log 2>&1
+
+
+##########################################################
+#				Apache Vhosts
+##########################################################
 # create project folder
 sudo chmod -R 755 /var/www
-sudo mkdir "/var/www/myfwk"
+sudo mkdir "/var/www/$SITENAME"
 
 # setup hosts file
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
-    DocumentRoot "/var/www/myfwk/public"
-    ServerName myfwk
-    <Directory "/var/www/myfwk/public">
+    ServerName $SITENAME.dev
+    DocumentRoot /var/www/$SITENAME/public
+    ErrorLog /var/www/$SITENAME/apache.error.log
+    CustomLog /var/www/$SITENAME/apache.access.log common
+    php_flag log_errors on
+    php_flag display_errors on
+    php_value error_reporting 2147483647
+    php_value error_log /var/www/$SITENAME/php.error.log
+   <Directory "/var/www/$SITENAME">
         AllowOverride All
         Require all granted
-    </Directory>
+   </Directory>
 </VirtualHost>
 EOF
 )
 echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
 
-sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/myfwk.conf
+sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$SITENAME.conf
 
-echo "${VHOST}" | sudo tee /etc/apache2/sites-available/myfwk.conf
+echo "${VHOST}" | sudo tee /etc/apache2/sites-available/$SITENAME.conf
 
-sudo a2ensite myfwk.conf
+sudo a2ensite $SITENAME.conf
 
 # enable mod_rewrite
 sudo a2enmod rewrite
 
-# install git
-sudo apt-get -y install git
-
+##########################################################
+#				Install Extras
+##########################################################
 # install Composer
 curl -s https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
-# Adding NodeJS from Nodesource. This will Install NodeJS Version 5 and npm
+sudo apt-get install snmp
+
+# Adding NodeJS from Nodesource. This will Install NodeJS and npm
 sudo apt-add-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs
@@ -88,4 +117,8 @@ sudo apt-get install -y nodejs
 sudo npm install -g bower gulp
 
 # restart apache
+sudo apt-get -y install libapache2-mod-php7.0
+sudo a2dismod php5
+sudo a2enmod php7.0
+
 service apache2 restart
